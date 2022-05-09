943a1a9714315ece8119549c6f0907bd464fb324
syx <2725596997@qq.com>
2022-05-09 16:44:14 +0800

    创建项目时提交

---
+++ lsjs-commons/src/test/java/com/syx/LsjsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsjsCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ lsjs-system/src/main/java/com/syx/LsjsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.syx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsjsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsjsSystemApplication.class, args);
+    }
+
+}
